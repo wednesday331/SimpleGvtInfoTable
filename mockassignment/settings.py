303327b87ba6aec4e033658b940c889085be1cc0
Shrey Shanbhag <wednesday331@gmail.com>
2020-09-19 14:28:12 -0400

    edit

--- mockassignment/settings.py
+++ mockassignment/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ye+rha!y2o41jgri$q98lb0p)d_g_6n8ket4z!m%@af+*9-b=%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['your_app_name.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
